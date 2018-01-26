0697b5b50889c860effc8ce93b62bd3d29d4673c
rbatsenko <rbatsenko@gmail.com>
2018-01-26 09:50:19 +0100

    links to see fading

--- js/main.js
+++ js/main.js
@@ -2,20 +2,21 @@ $(document).ready( function() {
     $('body').niceScroll({
         cursorwidth:"10px",
         cursorborder: "0px solid #fff",
-        zindex: [200]
+        zindex: [200],
+        smoothscroll: true
     });
     $(document).on( 'scroll', function() {
         scrollTop = $(window).scrollTop();
         $('#intro').css('background-position','50%' + '-' + scrollTop/1.5 + 'px');
         $('#intro .bg-add').css('background-position','-10%' + '-' + scrollTop/6 + 'px');
     })
-    $('body').fadeIn(200);
+    $('body').fadeIn(400);
     function newpage() {
         window.location = newLocation;
     }
     $("a").click(function(event){
         event.preventDefault();
         newLocation = this.href;
-        $("body").fadeOut(200, newpage);    
+        $("body").fadeOut(400, newpage);    
     });
 });
