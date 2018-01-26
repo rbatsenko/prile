4f82830e79059ca23f42836b3ad2ebfeb18e6d8b
rbatsenko <rbatsenko@gmail.com>
2018-01-26 17:29:53 +0100

    two more sections

--- js/main.js
+++ js/main.js
@@ -3,12 +3,22 @@ $(document).ready( function() {
         cursorwidth:"10px",
         cursorborder: "0px solid #fff",
         zindex: [200],
-        smoothscroll: true
+        smoothscroll: true,
+        scrollspeed: 80,
+        mousescrollstep: 20
     });
     $(document).on( 'scroll', function() {
         scrollTop = $(window).scrollTop();
+        height = $(window).height();
         $('#intro').css('background-position','50%' + '-' + scrollTop/1.5 + 'px');
         $('#intro .bg-add').css('background-position','-10%' + '-' + scrollTop/6 + 'px');
+        $('#how-it-works .bg-before-img').css('top', ( scrollTop*0.02 ) + -31 + '%');
+        $('#content-creators .bg-before-img').css('top', ( scrollTop*0.05 ) + 0 + 'px');
+        if ( scrollTop > 455 ) { 
+            $('.navbar').css('background-color','#095573'); 
+        } else {
+            $('.navbar').css('background-color','rgba(7, 51, 74, .2)');
+        };
     })
     $('body').fadeIn(400);
     function newpage() {
