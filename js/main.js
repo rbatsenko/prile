23d8dc4cdae91fb50d947c6d315587ff84a90c70
rbatsenko <rbatsenko@gmail.com>
2018-01-27 18:24:45 +0100

    some more css

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ $(document).ready( function() {
         $('#intro .bg-add').css('background-position','-10%' + '-' + scrollTop/6 + 'px');
         $('#how-it-works .bg-before-img').css('top', ( scrollTop*0.02 ) + -31 + '%');
         $('#content-creators .bg-before-img').css('top', ( scrollTop*0.05 ) + 0 + 'px');
-        if ( scrollTop > 455 ) { 
+        if ( scrollTop > 150 ) { 
             $('.navbar').css('background-color','#095573'); 
         } else {
             $('.navbar').css('background-color','rgba(7, 51, 74, .2)');
